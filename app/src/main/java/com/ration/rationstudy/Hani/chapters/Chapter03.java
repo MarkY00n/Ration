b71086ea13b86f96e737db9d6fa7f877f002bbd0
kimhanhee <h1008h@naver.com>
2018-11-11 18:19:22 +0900

    good

--- app/src/main/java/com/ration/rationstudy/Hani/chapters/Chapter03.java
+++ app/src/main/java/com/ration/rationstudy/Hani/chapters/Chapter03.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ration.rationstudy.Hani.common.BaseActivity;
 import com.ration.rationstudy.Hani.common.HaniAdapter;
@@ -23,6 +28,9 @@ import com.ration.rationstudy.http.RationCallback;
 import com.ration.rationstudy.http.response.StudentRepo;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -32,14 +40,19 @@ public class Chapter03 extends BaseActivity {
     Spinner spinner;
     EditText search_edt;
     Button search_btn;
+    ArrayList<StudentRepo> repos;
+    Stream<StudentRepo> stream;
+    Comparator<StudentRepo> compare;
+    int s_num = 0;
+    InputMethodManager imm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hani_activity_chapter03);
+        reqData();
         findViews();
         init();
-
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new StudentAdapter(this, R.layout.hani_item01);
         recyclerView.setAdapter(adapter);
@@ -48,7 +61,6 @@ public class Chapter03 extends BaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.hani_menu02, menu);
-        reqData();
         return true;
     }
 
@@ -56,12 +68,25 @@ public class Chapter03 extends BaseActivity {
         ApiService api = RationAPP.getRAPP().getRetrofit().create(ApiService.class);
         api.listStudentRepo().enqueue(new RationCallback<>(this, (call, response) -> {
             if (response.body() != null) {
-                ArrayList<StudentRepo> repos = response.body().getResult();
-                Stream<StudentRepo> stream;
-                stream = repos.stream().sorted();
-                adapter.setItemList((ArrayList<StudentRepo>) stream);
+                repos = response.body().getResult();
+                stream = repos.stream();
+                compare = Comparator.comparing(StudentRepo::getNumber);
+                adapter.setItemList((ArrayList<StudentRepo>) stream.sorted(compare).collect(Collectors.toList()));
             }
         }));
+    }
+
+    private void accData() {
+
+        Stream<StudentRepo> acc_stream = adapter.arrayList.stream();
+        compare = Comparator.comparing(StudentRepo::getNumber);
+        adapter.setItemList((ArrayList<StudentRepo>) acc_stream.sorted(compare).collect(Collectors.toList()));
+    }
+
+    private void descData() {
+        Stream<StudentRepo> des_stream = adapter.arrayList.stream();
+        compare = Comparator.comparing(StudentRepo::getNumber).reversed();
+        adapter.setItemList((ArrayList<StudentRepo>) des_stream.sorted(compare).collect(Collectors.toList()));
     }
 
 
@@ -78,17 +103,142 @@ public class Chapter03 extends BaseActivity {
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                s_num = position;
+                switch (position) {
+                    case 0: {
+                        stream = repos.stream();
+                        compare = Comparator.comparing(StudentRepo::getNumber);
+                        adapter.setItemList((ArrayList<StudentRepo>) stream.sorted(compare).collect(Collectors.toList()));
+                        hideKeyboard();
+                    }
+                    break;
+                    case 1: {
+                        search_edt.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        imm.showSoftInput(search_edt, 0);
+                    }
+                    break;
+                    case 2: {
+                        search_edt.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        imm.showSoftInput(search_edt, 0);
+                    }
+                    break;
+                    case 3: {
+                        search_edt.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        imm.showSoftInput(search_edt, 0);
+                    }
+                    break;
+                    case 4: {
+                        search_edt.setInputType(InputType.TYPE_CLASS_TEXT);
+                        imm.showSoftInput(search_edt, 0);
+                    }
+                    break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+    }
+
+    private void hideKeyboard() {
+        imm.hideSoftInputFromWindow(search_edt.getWindowToken(), 0);
     }
 
     @Override
     public void init() {
+        imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
         search_btn.setOnClickListener(v -> {
             try {
+                switch (s_num) {
+                    case 0: {
+                        Toast.makeText(this, "전체는 검색 할 수 없습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                    case 1: {
+                        if (search_edt.getText().toString().length() <= 0) {
+                            Toast.makeText(this, "값을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (!Pattern.matches("^[0-9]+$", search_edt.getText().toString())) {
+                            Toast.makeText(this, "숫자만 입력해주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        stream = repos.stream().filter((StudentRepo b) -> b.getNumber().startsWith(search_edt.getText().toString()));
+                        compare = Comparator.comparing(StudentRepo::getNumber);
+                        adapter.setItemList((ArrayList<StudentRepo>) stream.sorted(compare).collect(Collectors.toList()));
+                    }
+                    break;
+                    case 2: {
+                        if (search_edt.getText().toString().length() <= 0) {
+                            Toast.makeText(this, "값을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (!Pattern.matches("^[0-9]+$", search_edt.getText().toString())) {
+                            Toast.makeText(this, "숫자만 입력해주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        int edit_num = Integer.parseInt(search_edt.getText().toString());
+                        stream = repos.stream().filter((StudentRepo b) -> b.getYear() == edit_num);
+                        compare = Comparator.comparing(StudentRepo::getNumber);
+                        adapter.setItemList((ArrayList<StudentRepo>) stream.sorted(compare).collect(Collectors.toList()));
+                    }
+                    break;
+                    case 3: {
+                        if (search_edt.getText().toString().length() <= 0) {
+                            Toast.makeText(this, "값을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (!Pattern.matches("^[0-9]+$", search_edt.getText().toString())) {
+                            Toast.makeText(this, "숫자만 입력해주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        int edit_num = Integer.parseInt(search_edt.getText().toString());
+                        stream = repos.stream().filter((StudentRepo b) -> b.getGrade() == edit_num);
+                        compare = Comparator.comparing(StudentRepo::getNumber);
+                        adapter.setItemList((ArrayList<StudentRepo>) stream.sorted(compare).collect(Collectors.toList()));
+                    }
+                    break;
+                    case 4: {
+                        if (search_edt.getText().toString().length() <= 0) {
+                            Toast.makeText(this, "값을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        stream = repos.stream().filter((StudentRepo b) -> b.getPart().startsWith(search_edt.getText().toString()));
+                        compare = Comparator.comparing(StudentRepo::getNumber);
+                        adapter.setItemList((ArrayList<StudentRepo>) stream.sorted(compare).collect(Collectors.toList()));
+                    }
+                    break;
 
+                }
+                hideKeyboard();
             } catch (Exception e) {
 
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.asc_menu: {
+                accData();
+            }
+            break;
+            case R.id.desc_menu: {
+                descData();
+            }
+            break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     class StudentAdapter extends HaniAdapter<StudentRepo, StudentViewHolder> {
