b0d83fdaaca717553fdce6b251a94affcbe48db0
EunChani <kimbh0905@gmail.com>
2018-11-16 16:14:50 +0900

    Packaging Error Fixed

--- app/src/main/java/com/ration/rationstudy/marty/chapters/chapter1/Chapter4_marty.java
+++ app/src/main/java/com/ration/rationstudy/marty/chapters/chapter1/Chapter4_marty.java
@@ -7,13 +7,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Space;
 
+import com.jakewharton.rxbinding3.view.RxView;
 import com.ration.rationstudy.R;
+import com.ration.rationstudy.marty.common.martyBaseActivity;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Observable;
 
-public class Chapter4_marty extends AppCompatActivity {
+public class Chapter4_marty extends martyBaseActivity {
 
     @BindView(R.id.gallery_img)
     ImageView galleryImg;
@@ -22,13 +26,33 @@ public class Chapter4_marty extends AppCompatActivity {
     @BindView(R.id.next_btn)
     Button nextBtn;
 
+    int[] imgs;
+
+    int currentPos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter4_marty);
         ButterKnife.bind(this);
 
+        imgs = getResources().getIntArray(R.array.chapter4_imgs);
+        currentPos = imgs.length/2;
+        galleryImg.setImageResource(imgs[currentPos]);
 
+
+        Observable<Integer> prev = RxView.clicks(prevBtn).map(isNext->-1);
+        Observable<Integer> next = RxView.clicks(prevBtn).map(isNext->1);
+
+        Observable.merge(prev,next)
+                .subscribe(isNext->onMoveImages(isNext));
+    }
+
+
+    void onMoveImages(int isNext){
+        if (currentPos >= imgs.length) currentPos=0;
+        else if (currentPos < 0 ) currentPos = imgs.length-1;
+        currentPos += isNext;
+        galleryImg.setImageResource(imgs[currentPos]);
     }
 
 }
